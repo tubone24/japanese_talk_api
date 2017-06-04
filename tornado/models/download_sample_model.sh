eade774674ee3d11e11d5bd3f40cf5a4cf8b5ede
tubone <meitantei-conan@live.jp>
2017-06-04 21:43:25 +0900

    modify chainer1.6.1

--- tornado/models/download_sample_model.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget https://dl.dropboxusercontent.com/u/15715059/japanese_talk_api_models/japanese_talk_api.chainermodel
-wget https://dl.dropboxusercontent.com/u/15715059/japanese_talk_api_models/vocab.bin
